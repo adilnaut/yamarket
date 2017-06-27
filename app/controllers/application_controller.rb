0fbd4f281a56709decd5e5b59da8baf0245fe814
armansabyr <armansabyrov@gmail.com>
2017-06-27 06:16:19 +0000

    new parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+    
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  
+  
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :lastname, :phone, :email, :password, :password_confirmation) }
+        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :lastname, :phone, :email, :password, :remember_me) }
+    end
 end
